e82ea42ec01fbb8502d79c7749eac2ab92bf0b2b
bahaeddin <bahaeddin.feriani@gmail.com>
2022-08-14 18:48:53 +0000

    second commit

--- note-app.js
+++ note-app.js
@@ -1,53 +1,54 @@
-const yargs = require('yargs')
-const chalk = require('chalk')
-const program = require('./program.js')
+const yargs = require("yargs");
+const chalk = require("chalk");
+const program = require("./program.js");
 yargs.command({
-    command: 'add',
-    describe: 'add a note',
-    builder: {
-        title:{
-            describe: 'Title of note',
-            demandOption:'true',
-            type: 'string',
-        },
-        body:{
-            describe: 'Body of note',
-            demandOption:'true',
-            type: 'string',
-        }
+  command: "add",
+  describe: "add a note",
+  builder: {
+    title: {
+      describe: "Title of note",
+      demandOption: "true",
+      type: "string",
     },
-    handler: function (argv) {
-        program.ADD(argv.title,argv.body)
-}}
-)
+    body: {
+      describe: "Body of note",
+      demandOption: "true",
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    program.ADD(argv.title, argv.body);
+  },
+});
 
 yargs.command({
-    command: 'remove',
-    describe: 'Remove a note',
-    builder: {
-        title:{
-            describe: 'Title of note',
-            demandOption:'true',
-            type: 'string',
-        }},
-    handler: function(argv){
-        program.REMOVE(argv.title)
-    }
-    
-})
+  command: "remove",
+  describe: "Remove a note",
+  builder: {
+    title: {
+      describe: "Title of note",
+      demandOption: "true",
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    program.REMOVE(argv.title);
+  },
+});
 
 yargs.command({
-    command: 'list',
-    describe: 'List a note',
-    handler: function(){
-    program.LIST()
-}
-})
+  command: "list",
+  describe: "List a note",
+  handler: function () {
+    program.LIST();
+  },
+});
 
 yargs.command({
-    command: 'read',
-    describe: 'Read a note',
-    handler: function(argv){
-        program.READ(argv.title)}
-})
-yargs.parse()
+  command: "read",
+  describe: "Read a note",
+  handler: function (argv) {
+    program.READ(argv.title);
+  },
+});
+yargs.parse();
